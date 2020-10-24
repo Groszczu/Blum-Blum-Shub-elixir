c23ee66402b7d5210036a1603e7e9f8f42b13ed1
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-10-24 19:29:36 +0200

    Add coprime?/2 tests

--- test/numbers_test.exs
+++ test/numbers_test.exs
@@ -15,4 +15,18 @@ defmodule NumbersTest do
                |> Enum.any?(&prime?/1)
     end
   end
+
+  describe "coprime?/2" do
+    test "returns true for coprime numbers" do
+      assert true ==
+               [[3, 8], [5, 12], [15, 26], [33, 50]]
+               |> Enum.all?(fn [a, b] -> coprime?(a, b) end)
+    end
+
+    test "returns false for not coprime numbers" do
+      assert false ==
+               [[4, 8], [8, 12], [15, 24], [33, 48]]
+               |> Enum.any?(fn [a, b] -> coprime?(a, b) end)
+    end
+  end
 end
